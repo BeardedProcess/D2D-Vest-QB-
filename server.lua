15107d411ef1ee352a29d36dbb3550ecacf392ce
Bearded Process <97993063+BeardedProcess@users.noreply.github.com>
2022-04-24 00:49:00 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,31 +1,28 @@
 --MADE BY DareToDoyle#3835--
 
-ESX                = nil
+local QBCore = exports['qb-core']:GetCoreObject()
 
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
-ESX.RegisterUsableItem('bombvest', function(source)
+QBCore.Functions.CreateUseableItem('bombvest', function(source)
 
 	local _source = source
-    local xPlayer = ESX.GetPlayerFromId(_source)
-    xPlayer.removeInventoryItem('bombvest', 1)
+    local xPlayer = QBCore.Functions.GetPlayer(_source)
+    xPlayer.Functions.RemoveItem("bombvest", 1)
 	TriggerClientEvent('D2D-Vest:Use', source)
 	
 end)
 
-ESX.RegisterUsableItem('bombdefuse', function(source)
+QBCore.Functions.CreateUseableItem('bombdefuse', function(source)
 
 	local _source = source
-    local xPlayer = ESX.GetPlayerFromId(_source)
-    xPlayer.removeInventoryItem('bombdefuse', 1)
+    local xPlayer = QBCore.Functions.GetPlayer(_source)
+    xPlayer.Functions.RemoveItem("bombdefuse", 1)
 	TriggerClientEvent('D2D-Vest:Defuse', source)
 	
 end)
 
 RegisterServerEvent('D2D-Vest:PlayerExplosion')
 AddEventHandler('D2D-Vest:PlayerExplosion', function(coords)
-
     TriggerClientEvent("D2D-Vest:DetonateIfNearby", -1, coords)
-
 end)
+
 --MADE BY DareToDoyle#3835--
